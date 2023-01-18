97f0ea45b580d6ffba8b66079b957c5b1ebc0371
sumit <smtharijan@gmail.com>
2023-01-18 22:37:22 +0530

    optimized projects component

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,11 @@ import { ImInstagram } from "react-icons/im";
 import { BsGithub } from "react-icons/bs";
 import { AiFillEye } from "react-icons/ai";
 
+import { projects } from "./Resources/projects";
+
 function Projects() {
+  console.log(projects[1].links);
+
   return (
     <section>
       <div className="projects">
@@ -15,131 +19,69 @@ function Projects() {
           <h2 className="projectsTitle">
             <mark className="linear">Projects</mark>
           </h2>
-          <article className="project">
-            <div className="ProjectImg_Wrapper">
-              <img alt="project 1" className="projectImg" src={Project1} />
-            </div>
-            <div className="project-info">
-              <h1 className="ProjectTitle">
-                <mark className="linear">
-                  FRONTENDS PROJECTS | Ed-Tech Platform
-                </mark>
-              </h1>
-              <p className="projectDetail">
-                Created this platform to share projects with tutorial
-              </p>
-              <ul className="projectInfo social">
-                <li className="sociallist">
-                  <a
-                    className="social-icon"
-                    href="https://www.instagram.com/frontendsprojects/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <ImInstagram></ImInstagram>
-                  </a>
-                </li>
 
-                <li className="sociallist">
-                  <a
-                    className="social-icon"
-                    href="https://frontendsprojects.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <AiFillEye></AiFillEye>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </article>
+          {projects.map((projects, index) => {
+            const { img, title, subTitle, igUrl, gitUrl, viewUrl } = projects;
+            return (
+              <div className="project" key={index}>
+                <div className="ProjectImg_Wrapper">
+                  <img alt="project 1" className="projectImg" src={img} />
+                </div>
+                <div className="project-info">
+                  <h1 className="ProjectTitle">
+                    <mark className="linear">{title}</mark>
+                  </h1>
+                  <p className="projectDetail">{subTitle}</p>
+                  <ul className="projectInfo social">
+                    {igUrl ? (
+                      <li className="sociallist">
+                        <a
+                          className="social-icon"
+                          href={igUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <ImInstagram />
+                        </a>
+                      </li>
+                    ) : (
+                      " "
+                    )}
 
-          <article className="project">
-            <div className="ProjectImg_Wrapper">
-              <img alt="project 2" className="projectImg" src={Project2} />
-            </div>
-            <div className="project-info">
-              <h1 className="ProjectTitle">
-                <mark className="linear">CODINGSUMIT | Tech Blog</mark>
-              </h1>
-              <p className="projectDetail">
-                Created this blog to share web dev tutorial tips and tricks
-              </p>
-              <ul className="projectInfo social">
-                <li className="sociallist">
-                  <a
-                    className="social-icon"
-                    href="https://github.com/smthari"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <BsGithub></BsGithub>
-                  </a>
-                </li>
+                    {viewUrl ? (
+                      <li className="sociallist">
+                        <a
+                          className="social-icon"
+                          href={viewUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <AiFillEye />
+                        </a>
+                      </li>
+                    ) : (
+                      " "
+                    )}
 
-                <li className="sociallist">
-                  <a
-                    className="social-icon"
-                    href="https://www.instagram.com/codingsumit/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <ImInstagram></ImInstagram>
-                  </a>
-                </li>
-
-                <li className="sociallist">
-                  <a
-                    className="social-icon"
-                    href="https://codingsumit.com/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <AiFillEye></AiFillEye>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </article>
-
-          <article className="project">
-            <div className="ProjectImg_Wrapper">
-              <img alt="project 3" className="projectImg" src={Project3} />
-            </div>
-            <div className="project-info">
-              <h1 className="ProjectTitle">
-                <mark className="linear">
-                  CELEBRITY INFORMATION | API PROJECT
-                </mark>
-              </h1>
-              <p className="projectDetail">
-                Created this project using Ninja API
-              </p>
-              <ul className="projectInfo social">
-                <li className="sociallist">
-                  <a
-                    className="social-icon"
-                    href="https://github.com/smthari/celebrity-database"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <BsGithub></BsGithub>
-                  </a>
-                </li>
-
-                <li className="sociallist">
-                  <a
-                    className="social-icon"
-                    href="https://celebrity-database.netlify.app/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <AiFillEye></AiFillEye>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </article>
+                    {gitUrl ? (
+                      <li className="sociallist">
+                        <a
+                          className="social-icon"
+                          href={gitUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <BsGithub></BsGithub>
+                        </a>
+                      </li>
+                    ) : (
+                      " "
+                    )}
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
---
+++ src/Components/Resources/projects.js
@@ -0,0 +1,29 @@
+import Project1 from "../../Assets/Images/Project1.png";
+import Project2 from "../../Assets/Images/Project2.png";
+import Project3 from "../../Assets/Images/Project3.png";
+export const projects = [
+  {
+    id: 1,
+    title: "FRONTENDS PROJECTS | Ed-Tech Platform",
+    subTitle: "Created this platform to share projects with tutorial",
+    img: Project1,
+    igUrl: "https://www.instagram.com/frontendsprojects/",
+    viewUrl: "https://frontendsprojects.com/",
+  },
+  {
+    id: 2,
+    title: "FRONTENDS PROJECTS | Ed-Tech Platform",
+    subTitle: "Created this blog to share web dev tutorial tips and tricks",
+    img: Project2,
+    igUrl: "https://www.instagram.com/codingsumit/",
+    viewUrl: "https://codingsumit.com/",
+  },
+  {
+    id: 3,
+    title: "CELEBRITY INFORMATION | API PROJECT ",
+    subTitle: "Created this project using Ninja API ",
+    img: Project3,
+    gitUrl: "https://github.com/smthari/celebrity-database",
+    viewUrl: "https://celebrity-database.netlify.app/",
+  },
+];
