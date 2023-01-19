4e7143b9b441d0f8d4cc456b2b30cc3a52e4b811
sumit <smtharijan@gmail.com>
2023-01-19 09:04:26 +0530

    home component correction text

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,7 +13,7 @@ function Home() {
           <h2 className="intro-title">
             Hi, I’m <mark className="linear">Sumit</mark>
           </h2>
-          <h5 className="intro-subtitle">I’m a Frontend Dev</h5>
+          <h5 className="intro-subtitle">Frontend Developer</h5>
           <Social />
         </div>
       </div>
